b758f2183c95e29f494c51d2ccf959bfdaa887ef
Naila Rais <157850227+NailaRais@users.noreply.github.com>
2025-01-16 12:14:19 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,13 +4,12 @@ import numpy as np
 import fmodel
 
 # Argument parser
-parser = argparse.ArgumentParser(description="Predict the class of an image using a trained model")
-parser.add_argument('--image_path', default='./flowers/test/1/image_06752.jpg', type=str, help="Path to input image")
-parser.add_argument('--checkpoint', default='./checkpoint.pth', type=str, help="Path to model checkpoint")
+parser.add_argument('input', default='./flower_data/test/1/image_06752.jpg', nargs='?', type=str, help="Path to input image")
+parser.add_argument('--dir', default="./flower_data/", type=str, help="Path to data directory")
+parser.add_argument('checkpoint', default='./checkpoint.pth', nargs='?', type=str, help="Path to model checkpoint")
 parser.add_argument('--top_k', default=5, type=int, help="Number of top predictions to return")
 parser.add_argument('--category_names', default='cat_to_name.json', type=str, help="Path to JSON file mapping categories to names")
-parser.add_argument('--device', default="cpu", type=str, help="Device to use for inference ('cpu' or 'gpu')")
-parser.add_argument('--output_file', default=None, type=str, help="File to save prediction results")
+parser.add_argument('--gpu', default="gpu", type=str, help="Device to use for inference ('cpu' or 'gpu')")
 
 args = parser.parse_args()
 
